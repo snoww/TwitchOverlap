2389e7ea0a80c15cb888ecbc6dcc39b8270e4adf
Snow <snow@roki.sh>
2021-06-20 18:12:21 -0400

    testing out methods of serializing data trying different methods of processing data

--- ChannelIntersection/schema/stats.sql
+++ ChannelIntersection/schema/stats.sql
@@ -9,26 +9,8 @@ create table if not exists channel
     chatters     int,
     shared       int,
     last_update  timestamp
-);
+) with (fillfactor=95);
 
-create table if not exists overlap
-(
-    timestamp timestamp not null,
-    source    int references channel (id),
-    target    int references channel (id),
-    overlap   int       not null,
-    primary key (timestamp, source, target)
-);
-
-
-create index if not exists channel_timestamp_index on channel (last_update desc);
-
-create index if not exists overlap_timestamp_desc_index on overlap (timestamp desc);
-create index if not exists overlap_source_overlap_index on overlap (source, overlap desc);
-create index if not exists overlap_target_overlap_index on overlap (target, overlap desc);
-
-
----- new 
 
 create table if not exists overlap
 (
