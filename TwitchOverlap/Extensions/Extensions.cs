06e10f79ee17ecc8c769baa33dc51e68440bf099
Snow <snow@roki.sh>
2021-06-23 16:27:11 -0400

    update cache duration on the hour and half hour in case data calculation takes longer than a minute

--- TwitchOverlap/Extensions/Extensions.cs
+++ TwitchOverlap/Extensions/Extensions.cs
@@ -18,6 +18,10 @@ namespace TwitchOverlap.Extensions
         
         public static TimeSpan GetCacheDuration(this DateTime time)
         {
+            if (time.Minute is <= 5 or >= 30 and <= 35)
+            {
+                return TimeSpan.FromMinutes(2);
+            }
             int duration = (60 - time.Minute) % 30;
             return TimeSpan.FromMinutes(duration == 0 ? 1 : duration);
         }
