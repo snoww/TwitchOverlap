ca03cdf7f71ca5870af01aaeb7db9f3007f64dd0
snow <lz@snow.xyz>
2022-01-01 09:47:52 -0500

    update gtag for stats

--- TwitchOverlapWeb/pages/_app.tsx
+++ TwitchOverlapWeb/pages/_app.tsx
@@ -4,6 +4,8 @@ import {ThemeProvider} from "next-themes";
 import {Router} from "next/router";
 import NProgress from "nprogress";
 import "../styles/globals.css";
+import Script from "next/script";
+import React from "react";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -11,9 +13,25 @@ Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
-    <ThemeProvider attribute={"class"} defaultTheme={"dark"} enableSystem={false}>
-      {<Component {...pageProps} />}
-    </ThemeProvider>
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics*/}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-53NJ04WTT9"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+        
+                  gtag('config', 'G-53NJ04WTT9');
+                `}
+      </Script>
+      <ThemeProvider attribute={"class"} defaultTheme={"dark"} enableSystem={false}>
+        {<Component {...pageProps} />}
+      </ThemeProvider>
+    </>
   );
 }
 
--- TwitchOverlapWeb/pages/_document.tsx
+++ TwitchOverlapWeb/pages/_document.tsx
@@ -10,21 +10,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Global site tag (gtag.js) - Google Analytics*/}
-          {/*<script*/}
-          {/*  async*/}
-          {/*  src="https://www.googletagmanager.com/gtag/js?id=G-53NJ04WTT9"*/}
-          {/*/>*/}
-          {/*<script*/}
-          {/*  dangerouslySetInnerHTML={{*/}
-          {/*    __html: `*/}
-          {/*    window.dataLayer = window.dataLayer || [];*/}
-          {/*    function gtag(){dataLayer.push(arguments);}*/}
-          {/*    gtag('js', new Date());*/}
-          {/*  */}
-          {/*    gtag('config', 'G-53NJ04WTT9');`*/}
-          {/*  }}*/}
-          {/*/>*/}
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
                 integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
                 crossOrigin="anonymous"/>
