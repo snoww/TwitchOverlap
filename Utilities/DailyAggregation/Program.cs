68d304e1bdf16235ec9cc0edc2b753927fbe0830
snow <lz@snow.xyz>
2022-01-20 22:36:19 -0500

    track total time taken

--- Utilities/DailyAggregation/Program.cs
+++ Utilities/DailyAggregation/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace DailyAggregation;
+﻿using System.Diagnostics;
+
+namespace DailyAggregation;
 
 public static class Program
 {
     public static async Task Main()
     {
+        var sw = new Stopwatch();
         using var agg = new Aggregate();
         await agg.BeginAggregation();
+        Console.WriteLine($"total time taken: {sw.Elapsed:mm\\:ss}");
     }
 }
