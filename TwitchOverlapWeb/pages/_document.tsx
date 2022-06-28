209029525e8114b413dde854e466625c75c83dbd
snow <lz@snow.xyz>
2022-06-28 02:11:42 -0400

    add back font weights

--- TwitchOverlapWeb/pages/_document.tsx
+++ TwitchOverlapWeb/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={""}/>
-          <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"/>
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
                 integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
                 crossOrigin="anonymous"/>
